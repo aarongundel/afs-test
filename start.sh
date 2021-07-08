46d42cbe102ab797f41f00e26e0ddc2690504652
Aaron Gundel <aaron.gundel@gmail.com>
2021-07-08 12:30:40 -0600

    adds a start script

---
+++ start.sh
@@ -0,0 +1,10 @@
+# check for abitrary file to determine appropriate submodule pull
+FILE=./afs/manage.py
+if [ -f "$FILE" ]; then
+    git submodule update --init --recursive
+else
+    git submodule update --recursive --remote
+fi
+
+cd afs
+docker compose up
